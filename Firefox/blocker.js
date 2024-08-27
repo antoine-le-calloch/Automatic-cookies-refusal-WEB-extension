dbe74d4fb3832d3893b99e9df0da39b6098c3ea5
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-08-26 22:49:02 -0700

    Add some code example to execute.

---
+++ Firefox/blocker.js
@@ -0,0 +1,8 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        // Blocage des requêtes de cookies
+        return { cancel: true };
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking"]
+);
