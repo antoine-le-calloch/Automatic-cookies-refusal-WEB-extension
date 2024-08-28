dd9e29534b7c72f84c1e7fabb385e9f50cad864b
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-08-27 18:06:34 -0700

    Update code to click on Tarteau-citron deny all if button available.

--- Firefox/blocker.js
+++ Firefox/blocker.js
@@ -1,8 +1,14 @@
-browser.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        // Blocage des requêtes de cookies
-        return { cancel: true };
-    },
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-);
+function clickOnDenyAll() {
+    window.removeEventListener('tac.open_panel', clickOnDenyAll);
+    setTimeout(() => {
+        const denyAllButton = document.querySelector("button[id^='tarteaucitronAllDenied']");
+        if (denyAllButton) {
+            console.log("Tarteau-citron deny button found.");
+            denyAllButton.click();
+        } else {
+            console.log("No tarteau-citron deny button found.");
+        }
+    }, 1000);
+}
+
+window.addEventListener('tac.open_alert', clickOnDenyAll);
